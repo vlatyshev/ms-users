f5de89c224187e3b94bf4a4ddf9f538604a19fab
Victor Latyshev <v.latyshev@cappasity.com>
2021-08-13 18:17:19 +0300

    feat: revoke token api

---
+++ src/utils/revocation-list/sort-list.js
@@ -0,0 +1,21 @@
+/**
+ * sorting rules for fast find revoked token in rule list
+ * @param {Object} store
+ */
+const sortStoreList = (store) => {
+  // more logic for sorting?
+  const sortedList = Object.entries(store)
+    .sort((ruleA, ruleB) => {
+      const [, rulePropsA] = ruleA;
+      const [, rulePropsB] = ruleB;
+
+      const ruleClaimsSizeA = Object.keys(rulePropsA.claims);
+      const ruleClaimsSizeB = Object.keys(rulePropsB.claims);
+
+      return ruleClaimsSizeA - ruleClaimsSizeB;
+    });
+
+  return Object.fromEntries(sortedList);
+};
+
+module.exports = sortStoreList;
